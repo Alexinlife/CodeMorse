d0fc98b719522ff47a48295845cdecb6e9d2cf88
Alexinlife <alexinlife.exe@gmail.com>
2019-03-27 14:13:18 -0400

    Ajoute les lettres j à n en morse

---
+++ j.py
@@ -0,0 +1,21 @@
+from gpiozero import LED
+from time import sleep
+
+led = LED(17)
+
+led.On()
+sleep(0.25)
+led.Off()
+sleep(0.25)
+led.On()
+sleep(0.75)
+led.Off()
+sleep(0.25)
+led.On()
+sleep(0.75)
+led.Off()
+sleep(0.25)
+led.On()
+sleep(0.75)
+led.Off()
+sleep(0.75)
