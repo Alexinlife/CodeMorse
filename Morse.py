53580a02fab850ce4e3aa057a516d40e028a20c1
Alexinlife <alexinlife.exe@gmail.com>
2019-03-28 16:55:28 -0400

    Ajoute le programme principal

---
+++ Morse.py
@@ -0,0 +1,93 @@
+import os
+
+requete = input("Que voulez-vous dire? >")
+requete = requete.lower()
+cmpt = 0
+tabChar = []
+
+for element in requete:
+    tabChar.append(requete[cmpt])
+    cmpt += 1
+
+cmpt = 0
+
+for element in tabChar:
+    if (tabChar[cmpt] == 'a'):
+        os.system('a.py')
+
+    if (tabChar[cmpt] == 'b'):
+        os.system('b.py')
+
+    if (tabChar[cmpt] == 'c'):
+        os.system('c.py')
+
+    if (tabChar[cmpt] == 'd'):
+        os.system('d.py')
+
+    if (tabChar[cmpt] == 'e'):
+        os.system('e.py')
+
+    if (tabChar[cmpt] == 'f'):
+        os.system('f.py')
+
+    if (tabChar[cmpt] == 'g'):
+        os.system('g.py')
+
+    if (tabChar[cmpt] == 'h'):
+        os.system('h.py')
+
+    if (tabChar[cmpt] == 'i'):
+        os.system('i.py')
+
+    if (tabChar[cmpt] == 'j'):
+        os.system('j.py')
+
+    if (tabChar[cmpt] == 'k'):
+        os.system('k.py')
+
+    if (tabChar[cmpt] == 'l'):
+        os.system('l.py')
+
+    if (tabChar[cmpt] == 'm'):
+        os.system('m.py')
+
+    if (tabChar[cmpt] == 'n'):
+        os.system('n.py')
+
+    if (tabChar[cmpt] == 'o'):
+        os.system('O.py')
+
+    if (tabChar[cmpt] == 'p'):
+        os.system('P.py')
+
+    if (tabChar[cmpt] == 'q'):
+        os.system('Q.py')
+
+    if (tabChar[cmpt] == 'r'):
+        os.system('R.py')
+
+    if (tabChar[cmpt] == 's'):
+        os.system('S.py')
+
+    if (tabChar[cmpt] == 't'):
+        os.system('T.py')
+
+    if (tabChar[cmpt] == 'u'):
+        os.system('U.py')
+
+    if (tabChar[cmpt] == 'v'):
+        os.system('V.py')
+
+    if (tabChar[cmpt] == 'w'):
+        os.system('W.py')
+
+    if (tabChar[cmpt] == 'x'):
+        os.system('X.py')
+
+    if (tabChar[cmpt] == 'y'):
+        os.system('Y.py')
+
+    if (tabChar[cmpt] == 'z'):
+        os.system('Z.py')
+
+    cmpt += 1
